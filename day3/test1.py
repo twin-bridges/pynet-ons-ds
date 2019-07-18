1c381e81ec160b54727471b6e2c288c6e6b3f929
Kirk Byers <ktbyers@twb-tech.com>
2019-07-18 15:23:09 -0700

    Adding test1 to cause travis failure

---
+++ day3/test1.py
@@ -0,0 +1,17 @@
+from subproc_func import subprocess_wrapper
+import re
+
+cmd_list = ["df", "-h"]
+std_out, std_err, return_code = subprocess_wrapper(cmd_list)
+
+output = re.sub(r"Filesystem.*on", "", std_out) 
+match = re.findall(r"^(\S+)\s*.*?(\d+%)\s.*$", output, flags=re.M)
+
+my_list = []
+for file_system, usage in match:
+    new_dict = {}
+    new_dict["filesystem"] = file_system
+    new_dict["usage"] = usage
+    my_list.append(new_dict)
+
+print(my_list)
